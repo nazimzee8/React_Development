6101212e4704970e84b0ff72fdc31936c1516f2c
nazimzee8 <nazerrouki@gmail.com>
2024-01-16 19:29:17 -0800

    Finishing up content for webpage.

--- Nazim's_Portfolio/my-portfolio/src/App.js
+++ Nazim's_Portfolio/my-portfolio/src/App.js
@@ -206,32 +206,34 @@ function RightBorder() {
 const PurposePage = () => {
   return (
     <div class="purpose_page" id="purpose_page">
-      <h2>Purpose</h2>
-      <br/>
-      <h3> <p>I enjoyed my time developing multiple programming skills at Univerisity of Washington, Tacoma.
-        However, there were also numerous difficulties that I had to overcome; some of which left me questioning
-        if I was ever going to graduate and succeed. This feeling of doubt and unieasiness was something that he begun 
-        during my first year at University of Washington which had persisted during the time I was enrolled in their
-        Computer Science program. My first programming course was an introduction to programming where I first learned 
-        how to program in python. There were some students that also struggled but many others who grasped these concepts
-        relatively quickly whereas for me, it felt like a foreign language; incomprehensible. </p>
+      <div class="purpose_section">
+        <h2>Purpose</h2>
+        <br/>
+        <h3> <p>I enjoyed my time developing multiple programming skills at Univerisity of Washington, Tacoma.
+          However, there were also numerous difficulties that I had to overcome; some of which left me questioning
+          if I was ever going to graduate and succeed. This feeling of doubt and unieasiness was something that he begun 
+          during my first year at University of Washington which had persisted during the time I was enrolled in their
+          Computer Science program. My first programming course was an introduction to programming where I first learned 
+          how to program in python. There were some students that also struggled but many others who grasped these concepts
+          relatively quickly whereas for me, it felt like a foreign language; incomprehensible. </p>
 
-        <p>I was typically an A student who made it to the Deans' list within the majority of my academic career and yet 
-        I scored one of the lowest grades I had ever received in a class, a 3.0. Towards the end of the class, I was actually
-        relieved that it was finally over. None of the material that was discussed in the class made any sense to me.
-        Creating my own for loops to solve a problem without hardcoding it was perplexing to me, I didn't even remotely understand
-        how to parse through a file, and I didn't understand how to construdct my own functions to effectively solve a problem
-        in respect to different parameters.</p>
+          <p>I was typically an A student who made it to the Deans' list within the majority of my academic career and yet 
+          I scored one of the lowest grades I had ever received in a class, a 3.0. Towards the end of the class, I was actually
+          relieved that it was finally over. None of the material that was discussed in the class made any sense to me.
+          Creating my own for loops to solve a problem without hardcoding it was perplexing to me, I didn't even remotely understand
+          how to parse through a file, and I didn't understand how to construdct my own functions to effectively solve a problem
+          in respect to different parameters.</p>
 
-        <p>While I didn't understand it at the time, programming is very similar to math; not in the sense of rationalizing
-        complex problems using a set of rules. It's all about comparmentalizing a problem and interpreting each piece in a way
-        that is understandable to you via pseudocode. This is often done by thinking simplistically about what needs to be done before
-        translating it into code.</p>
+          <p>While I didn't understand it at the time, programming is very similar to math; not in the sense of rationalizing
+          complex problems using a set of rules. It's all about comparmentalizing a problem and interpreting each piece in a way
+          that is understandable to you via pseudocode. This is often done by thinking simplistically about what needs to be done before
+          translating it into code.</p>
 
-        <p>This wasn't the only time I struggled. My last year at University of Washington was especially a difficult one where
-        I worked in a group on a big project that was especially difficult to work with. There are things that I had learned
-        throughout my academic career to help me succeed throughout my undergraduate and graduate ventures at University of Washington.</p>
-        </h3>
+          <p>This wasn't the only time I struggled. My last year at University of Washington was especially a difficult one where
+          I worked in a group on a big project that was especially difficult to work with. There are things that I had learned
+          throughout my academic career to help me succeed throughout my undergraduate and graduate ventures at University of Washington.</p>
+          </h3>
+      </div>
     </div>
   )
 }
@@ -291,6 +293,8 @@ const AboutPage = (props) => {
     </div>
   )
 }*/
+/*https://th.bing.com/th/id/OIP.KJd-DqNgLur33Btzcsg3KQHaHA?rs=1&pid=ImgDetMain
+https://www.pngfind.com/mpng/ihRRxmR_communication-integrated-marketing-communication-icon-hd-png-download/*/
 
 const SkillsPage = () => {
   return (
@@ -298,13 +302,59 @@ const SkillsPage = () => {
       <h2>Skills</h2>
       <br/>
       <div class="skills_section">
-
         <div class="hard_skills">
-          <div class="h_block">Hard Skills</div>
+          <h3>Hard Skills</h3>
+          <img src="https://static.vecteezy.com/system/resources/previews/014/541/527/original/administrator-system-icon-simple-style-vector.jpg"></img>
+          <div class="hskill_container">
+          <h4>Specific skills, competencies, knowledge, and abilities of programming languages, frameworks,
+              and APIs that can be leveraged for developing software applications. </h4>
+          </div>
+          <div class="hard_skills_section">
+            <h4 style={{fontWeight: "bold"}}> Hard Skills:</h4>
+            <h4>
+              <ul>
+                <li>Object Oriented Programming (Java, Python, JavaScript, C)</li>
+                <li>Algorithms (Dynammic Programming, Recursion) </li>
+                <li>Database Design (Relational Databases, SQL, OracleDB, Microsoft Azure)</li>
+                <li>Web Development (HTML/CSS, JavaScript, Node.js, Reactjs)</li>
+                <li>Machine Learning (Neural Networks, Model-based, Model-free)</li>
+                <li>Web Services (Node-red, Rest API (Java), Flask (Python))</li>
+                <li>Data Science</li>
+                <li>Full-Stack Development (HTML/CSS, JavaScript, Node.js, Reactjs, PHP) </li>
+                <li>Artificial Intelligence</li>
+                <li>Cloud Computing (GCP, AWS, Azure)</li>
+                <li>Mobile App Development (Java)</li>
+              </ul>
+            </h4>
+          </div>
         </div>
 
         <div class="soft_skills">
-          <div class="s_block">Soft Skills</div>
+          <h3>Soft Skills</h3>
+          <img src="https://i.pinimg.com/736x/d7/1b/70/d71b703a681d8674bec810570ab1809a.jpg"></img>
+          <div class="sskill_container">
+            <h4>Personality traits, behaviors, interpersonal skills, and social competencies that can be 
+              used while managing tasks and interacting with others when developing projects.</h4>
+          </div>
+          <div class="soft_skills_section">
+            <h4 style={{fontWeight: "bold"}}> Soft Skills:</h4>
+            <h4>
+              <ul>
+                <li>Communication</li>
+                <li>Collaboration</li>
+                <li>Teamwork</li>
+                <li>Research</li>
+                <li>Friendliness</li>
+                <li>Adaptability</li>
+                <li>Prioritization</li>
+                <li>Time Management</li>
+                <li>Problem Solver</li>
+                <li>Flexible</li>
+                <li>Self-Motivated</li>
+                <li>Leadership Skills</li>
+              </ul>
+            </h4>
+          </div>
         </div>
       </div>
     </div>
@@ -313,13 +363,69 @@ const SkillsPage = () => {
 
 const TipsPage = () => {
   return (
-    <div class="tips_page" id="tips_page"></div>
+    <div class="tips_page" id="tips_page">
+      <h2>Tips & Strategies</h2>
+      <div class="tips_section">
+        <img class="collab" src="https://nazimz99.github.io/UW-CS_Experience/img/teamwork.png"/>
+        <ul>
+          <li>
+            <h3><u>Collaborate with others.</u>&ensp;
+              Forming tight-knit groups using various different channels such as Zoom and Discord is an excellent way to gain additional support and learn from others. 
+              This varies across schools, but as long as the work that is produced is your own, any collaborative work is welcome. 
+              Through my experience, I found that collaboration has had a profound impact on the work that I produced. 
+              It will also better prepare you for the real world.</h3>
+          </li>
+          <li>
+            <h3><u>Don't take notes.</u> &ensp; It's more important to pay attention to the concepts that are taught in class rather than mindlessly writing everything down. Professors will typically record their lectures, so that crucial explanations can be revisited at your own leisure. 
+            Paying attention to distinct words can help you uncover things that written notes couldn't.</h3>
+          </li>
+          <img class="break" src="https://nazimz99.github.io/UW-CS_Experience/img/break.png"/>
+          <li>
+            <h3><u>Break a problem into pieces.</u> &ensp; Compartmentalize. Understand what is required for the problem and think about what classes, interfaces, and functions are needed to accomplish the task. 
+            If you need a data structure to return the game state of a board, write a function for it. 
+            If you need to update the board, write a function for it. 
+            This way, when you understand what needs to be done, you can simply call that function and focus your time on the next task. 
+            This also makes it easier to focus and not be overwhelmed.</h3>
+          </li>
+          <li>
+            <h3><u>Learn to be resourceful.</u> &ensp;  
+            Collaboration is crucial, but one of the most important things I learned is that Google is your best friend. 
+            In Computer Security, I was required to complete a variety of esoteric labs found on Seed Labs and the most important thing was not whether or not I understood Cybersecurity. 
+            It's the fact that I was willing to learn and was willing to continue searching through the Web until I found the information that I needed. 
+            This also enabled me to create a web page and connect it to an advanced relational database with the help of a partner despite having no prior experience.
+            That can be located on my <a style = {{cursor: "pointer", textDecorationColor: "pink"}} onClick={() => window.location.href = "https://github.com/nazimzee8"}>github</a>.</h3>
+          </li>
+          <img class="diagram" src="https://nazimz99.github.io/UW-CS_Experience/img/General-Tree-Structure.png"/>
+          <li>
+            <h3><u>Use visuals to help.</u> &ensp; 
+            Creating visual diagrams is very useful for analyzing how a data structure works or discerning a pattern in an algorithm.</h3>
+          </li>
+          <li><h3><u>Use inductive reasoning. </u> &ensp; 
+          Learning how to make generalizations by analyzing a sub-problem and 
+          applying it to the entire problem as a whole 
+          is very useful in simplifying and envisioning a problem. 
+          Don't focus on the entire problem, create a sub-problem that is manageable.</h3></li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
 const ContactsPage = () => {
   return (
-    <div class="contacts_page" id="contacts_page"></div>
+    <div class="contacts_page" id="contacts_page">
+      <h4> Get Connected </h4>
+      <h2>Contact Me</h2>
+      <div class="contacts_section">
+        <div class="email">
+          <img src="https://th.bing.com/th/id/OIP.Z2lotQUlRFaoqY7m3Ma89QHaHa?rs=1&pid=ImgDetMain"></img>
+          <a onClick={() => window.location.href = "mailto:nazerrouki@gmail.com"}><h3>nazerrouki@gmail.com</h3></a>
+        </div>
+        <div class="linkedin">
+          <img src="https://www.xenneotech.com/images/linked-in.png"></img>
+          <h3>LinkedIn</h3></div>
+      </div>
+    </div>
   )
 }
 
